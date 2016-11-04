8ec784d50bb00ccdaee3a75f440d11cae2f4ed45
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-11-04 12:38:56 -0500

    added instance of Belt

--- Main.java
+++ Main.java
@@ -4,16 +4,18 @@ class Main {
     public static void main(String[] args) { 
 
         // No visualizer yet
-        Floor floor = new Floor();
+        Floor     floor     = new Floor();
         Inventory inventory = new Inventory();
-        Orders orders = new Orders();
-        Robot robot = new Robot();
+        Orders    orders    = new Orders();
+        Belt      belt      = new Belt();
+        Robot     robot     = new Robot();
 
         for (int i = 0;; ++i) {
             floor.tick(i);
             inventory.tick(i);
             orders.tick(i);
-            robot.tick(i);
+            belt.tick(i);
+            robot.tick(i); 
         }
     }
 }
