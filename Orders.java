9dbdb4ae15d5afddf11a0b9189709e85a50a817b
TylerFoster23 <tyler-foster@uiowa.edu>
2016-11-04 17:01:20 -0500

    Add files via upload
    My current work for Orders.java.

---
+++ Orders.java
@@ -0,0 +1,37 @@
+package orders;
+
+import java.util.ArrayList;
+
+public class Orders {
+    
+    public class Item {
+        public Item(){}
+    }
+    
+    public class Order {
+        ArrayList<Item> items;
+        String status;
+        
+        public Order(){
+            items = new ArrayList<>();
+            status = "not started";
+        }
+        
+        public void addItem(Item a){
+            items.add(a);
+        }
+        
+        public void removeItem(Item a){
+            items.remove(a);
+        }
+        
+        public void contains(Item a){
+            items.contains(a);
+        }
+        
+        public ArrayList<Item> getItems(){
+            return items;
+        }
+        
+    }
+}
