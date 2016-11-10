44664f1ff07e62701451617c9139c516aa91ae45
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-11-10 14:15:35 -0600

    getters and setters created

--- Item.java
+++ Item.java
@@ -1,4 +1,21 @@
 public class Item {
-    int     id_number;
-    String  name;
+    private int     id_number;
+    private String  name;
+
+    public void set_id_number(int num) {
+        this.id_number = num;
+    }
+
+    public int get_id_number() {
+        return this.id_number;
+    }
+
+    public void set_name(String new_name) {
+        this.name = new_name;
+    }
+
+    public String get_name() {
+        return this.name;
+    }
+
 }
