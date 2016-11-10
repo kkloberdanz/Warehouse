516ef0ebab4c29371765db5c56319a37f9396569
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-11-10 14:43:39 -0600

    renamed from Inventory1

--- Inventory.java
+++ Inventory.java
@@ -15,27 +15,47 @@ public class Inventory {
     int size;
     int weight;
     int quantity;
-    ArrayList<Inventory> database;
+    HashMap<Integer,Inventory> database;
     Boolean reachcapacity=false;
-    public Inventory(String item,int id,int size,int weight){
+    public Inventory(String item,int id,int size,int weight,int quantity){
         this.item=item;
         this.id=id;
         this.size=size;
         this.weight=weight;
+        this.quantity=quantity;
     }
-    public void initialize(String[] item,int[] id,int[] size,int[] weight){
+    public void initialize(String[] item,int[] id,int[] size,int[] weight,int[] quantity){
        for(int i=0;i<item.length;i++){
-           Inventory initial=new Inventory(item[i],id[i],size[i],weight[i]);
-           
+           Inventory initial=new Inventory(item[i],id[i],size[i],weight[i],quantity[i]);
+           additem(initial);
+           putitemonshelf(initial);
        }
     }
     public void additem(Inventory item){
-        
+        if(database.containsKey(item.id)){
+            database.get(item.id).quantity+=item.quantity;
+        }
+        else{
+            database.put(item.id,item);
+        }
     }
     public void putitemonshelf(Inventory item){
-        
+                                    
     }
     public void removeitem(Inventory item){
+        if(!database.containsKey(item.id)){
+            System.out.println("We don't have this product in stock");
+        }
+        else{
+            if(database.get(item.id).quantity<item.quantity){
+                System.out.println("We don't have enough products in stock, please add more products or reduce the number of products to be shippped");
+            }
+            else{
+                database.get(item.id).quantity-=item.quantity;
+            }
+        }
+    }
+    public void removeitemonshelf(Inventory item){
         
     }
     
