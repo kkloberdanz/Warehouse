092f24ed86bf99e0597aed82e26322978bc68a5f
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-11-10 14:35:51 -0600

    added constructor and toString

--- Item.java
+++ Item.java
@@ -2,6 +2,15 @@ public class Item {
     private int     id_number;
     private String  name;
 
+    public Item(int num, String new_name) {
+        this.id_number = num;
+        this.name = new_name;
+    }
+
+    public Item(String new_name, int num) {
+        this(num, new_name);
+    }
+
     public void set_id_number(int num) {
         this.id_number = num;
     }
@@ -16,6 +25,10 @@ public class Item {
 
     public String get_name() {
         return this.name;
+    }
+
+    public String toString() {
+        return id_number + ", " + name;
     }
 
 }
