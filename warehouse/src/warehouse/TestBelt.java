89cba52f8f40931883e3e0086a4ec890b9e42d82
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-11-14 17:55:47 -0600

    added tests

---
+++ warehouse/src/warehouse/TestBelt.java
@@ -0,0 +1,14 @@
+package warehouse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBelt {
+	
+	@Test
+	public void testPackBin() {
+		;
+	}
+
+}
---
+++ warehouse/src/warehouse/TestMockFloor.java
@@ -0,0 +1,42 @@
+package warehouse;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.awt.Point;
+
+public class TestMockFloor {
+
+	@Test
+	public void testGetPicker() {
+		MockFloor floor = new MockFloor();
+		assertTrue(floor.getPicker() instanceof Point);
+	}
+	
+	@Test
+	public void testGetPacker() {
+		MockFloor floor = new MockFloor();
+		assertTrue(floor.getPacker() instanceof Point);
+	}
+	
+	@Test
+	public void testGetShippingDock() { 
+		MockFloor floor = new MockFloor();
+		assertTrue(floor.getShippingDock() instanceof Point);
+	}
+
+    @Test
+    public void testGetReceivingDock() {
+		MockFloor floor = new MockFloor();
+		assertTrue(floor.getReceivingDock() instanceof Point);
+    }
+
+    @Test
+    public void testGetChagers() {
+		MockFloor floor = new MockFloor();
+        ArrayList<Point> arr = new ArrayList(floor.getChargers());
+        for (Point p : arr) {
+            assertTrue(p > 0);
+        }
+    }
+}
