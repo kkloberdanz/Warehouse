03cab44302652773ac316cbcf3779b3f7115ec67
Tyler <Tyler@Tylers-PC>
2016-11-14 14:21:16 -0600

    Added javadoc

--- warehouse/src/warehouse/Belt.java
+++ warehouse/src/warehouse/Belt.java
@@ -2,7 +2,10 @@ package warehouse;
 
 import java.util.ArrayList;
 
-
+/**
+ * Belt class for warehouse project. Moves items along the belt
+ * @author Tyler Sporrer
+ */
 
 public class Belt implements Tick{
 	
@@ -11,6 +14,10 @@ public class Belt implements Tick{
 	
 	int tick = 0;
 	
+	/**
+	 * packBin method takes order id and puts it into an Array list. When Bin gets to the 10th position, Boxes item and sends to shipping.
+	 * @author Tyler Sporrer
+	 */
 	public void packBin(int x){
 		int y;
 		belt.add(x);
@@ -23,18 +30,30 @@ public class Belt implements Tick{
 		}	
 	}
 	
+	/**
+	 * showBelt method shows the the order ID of the bins on the belt
+	 * @author Tyler Sporrer
+	 */
 	public void showBelt(){
 		for(int i = 0; i < belt.size(); i++){
 			System.out.println(belt.get(i));
 		}
 	}
 	
+	/**
+	 * showShip method shows the order ID of the orders that have been packed and sent to shipping
+	 * @author Tyler Sporrer
+	 */
 	public void showShip(){
 		for(int i = 0; i < ship.size(); i++){
 			System.out.println(ship.get(i));
 		}
 	}
 	
+	/**
+	 * tick method will use the previous methods when called.
+	 * @author Tyler Sporrer
+	 */
 	public void tick(int x){
 		
 	}
