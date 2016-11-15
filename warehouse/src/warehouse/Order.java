0c82037612b5fb6cd1c809c48c51b2f1b521d06a
TylerFoster23 <tyler-foster@uiowa.edu>
2016-11-15 11:03:27 -0600

    Added new methods to Order

--- warehouse/src/warehouse/Order.java
+++ warehouse/src/warehouse/Order.java
@@ -10,14 +10,46 @@ import java.util.ArrayList;
 
 public class Order implements Tick {
 	
-	String orderAddress;
-	OrderItem[] orderItems;
-	boolean isFilled;
+	String orderAddress;			// address of this order
+	String orderName;				// who this order is going to
+	ArrayList<Item> orderItems;		// list of items included in order
+	boolean isFilled;				// has the order been filled?
 	
-	public Order (String address, OrderItem[] items){
+	public Order (String address, String name, ArrayList<Item> items){
 		orderAddress = address;
+		orderName = name;
 		orderItems = items;
 		isFilled = false;
+	}
+	
+	// Adds an item to the list of items required for this order
+	public void addItem(Item a) {
+		orderItems.add(a);
+	}
+	
+	// Removes the item from the list of items required for this order
+	public void removeItem(Item a) {
+		orderItems.remove(a);
+	}
+	
+	// Queries on whether or not the item is required for this order
+	public boolean containsItem(Item a) {
+		orderItems.contains(a);
+	}
+	
+	// Returns the address the order is being shipped to
+	public String getAddress() {
+		return orderAddress;
+	}
+	
+	// Returns the name of the person who placed the order
+	public String getName() {
+		return orderName;
+	}
+	
+	// Returns the list of items within this order
+	public ArrayList<Item> getItems() {
+		return orderItems;
 	}
 }
     
