3de797c09af4dd78ab97ede610b552c45c5f0f08
TylerFoster23 <tyler-foster@uiowa.edu>
2016-11-14 23:27:30 -0600

    Add files via upload

--- warehouse/src/warehouse/Orders.java
+++ warehouse/src/warehouse/Orders.java
@@ -1,37 +1,11 @@
 package warehouse;
 
-import java.util.ArrayList;
+/**
+ * 
+ * @author Tyler
+ *
+ */
 
-public class Orders {
-    
-    public class Item {
-        public Item(){}
-    }
-    
-    public class Order {
-        ArrayList<Item> items;
-        String status;
-        
-        public Order(){
-            items = new ArrayList<>();
-            status = "not started";
-        }
-        
-        public void addItem(Item a){
-            items.add(a);
-        }
-        
-        public void removeItem(Item a){
-            items.remove(a);
-        }
-        
-        public void contains(Item a){
-            items.contains(a);
-        }
-        
-        public ArrayList<Item> getItems(){
-            return items;
-        }
-        
-    }
+public interface Orders {
+
 }
